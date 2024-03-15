af85ab08538bcdda46d13711c7c08e30fe487a0d
Sergio Andres Jaramillo Luna <sjaramilloja@summa-sci.com>
2024-03-14 20:47:48 -0500

    image lazy loading

--- src/app/gifs/components/card/card.component.ts
+++ src/app/gifs/components/card/card.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-
 import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-card',
-  templateUrl: './card.component.html',
+  templateUrl: './card.component.html'
 })
 export class CardComponent implements OnInit {
 
@@ -12,8 +11,7 @@ export class CardComponent implements OnInit {
   public gif!: Gif;
 
   ngOnInit(): void {
-    if ( !this.gif ) throw new Error('Gif property is required')
+    if ( !this.gif ) throw new Error('Gif property is required');
   }
-
 
 }
